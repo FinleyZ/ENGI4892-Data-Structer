98fddc027d5e36134cf71e42d29812c4f8aec864
Finley <zhf79212@gmail.com>
2020-05-27 19:34:10 -0230

    Exercise for Week3

---
+++ Exercise4/cmake-build-debug/CMakeFiles/Exercise4.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Exercise4.dir/sorting.cpp.o"
+  "Exercise4"
+  "Exercise4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Exercise4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
