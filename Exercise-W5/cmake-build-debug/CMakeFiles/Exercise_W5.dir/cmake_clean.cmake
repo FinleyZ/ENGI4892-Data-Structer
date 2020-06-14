350f8d4165a180cd748ac948c868c07a6d25d263
Finley <zhf79212@gmail.com>
2020-06-14 00:24:00 -0230

    week 5

---
+++ Exercise-W5/cmake-build-debug/CMakeFiles/Exercise_W5.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Exercise_W5.dir/main.cpp.o"
+  "Exercise_W5"
+  "Exercise_W5.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Exercise_W5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- Exercise-W5/main.cpp
+++ Exercise-W5/main.cpp
@@ -1,15 +1,43 @@
 #include <iostream>
+#include <vector>
+using namespace std;
 
+
+
+//initialize an int variable in stack.
 void recursive(int n){
     if (n == 0)
         return;
     int a;
     printf("%p\n", &a);
-    recursive(n - 1);
+    recursive(n - 1);//recursion of recursive()
+}
+
+int RPN_Expression(vector<char> vector){
+    int hold = 0;
+    for (auto it  = vector.begin() ; it != vector.end(); ++it) {
+        std::cout<< *it <<"\n";
+
+    }
+    std::cout<<"hi";
+    return 0;
 }
 
 int main() {
-    recursive(5);
+    //Demonstrate the stack
+    //recursive(5);
+
+    std::vector<char> expression = {'1','2','+','3','*'};
+
+    RPN_Expression(expression);
+
+
+
+//    for (auto i = expression.begin() ; i != expression.end(); ++i) {
+//        std::cout<< *i <<"\n";
+//
+//    }
+
 
 
 }
